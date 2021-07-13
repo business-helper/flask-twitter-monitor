c0478ef5974c55db6cdd45a1aa6a7a5e0d3b6609
Austin <aripang.com@gmail.com>
2021-07-13 10:33:52 +0800

    scheduler;

--- marketingBot/controllers/cron.py
+++ marketingBot/controllers/cron.py
@@ -1,11 +1,17 @@
-from flask_crontab import Crontab
+# from flask_crontab import Crontab
 from marketingBot import app
+from apscheduler.schedulers.background import BackgroundScheduler
 
-crontab = Crontab(app)
+# crontab = Crontab(app)
 
-@crontab.job(minute = "1")
-def run_schedule_bots():
-  print('[Run Schedule Bot]')
+# @crontab.job(minute = "1")
+# def run_schedule_bots():
+#   print('[Run Schedule Bot]')
 
+def test_job():
+    print('I am working...')
 
+scheduler = BackgroundScheduler()
+job = scheduler.add_job(test_job, 'interval', minutes=1)
+scheduler.start()
 
